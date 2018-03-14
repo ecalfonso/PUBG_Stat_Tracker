22da44cc190d00fcd91b0ecb205680237b330452
Eduard A <xsynergy510x@gmail.com>
2018-03-14 11:28:52 -0700

    Initial Commit - Code already completed
    Windows Form Program created in Visual Studio 2017 Express
    
    Form listens to Keyboard presses in the background. Implementation taken from https://stackoverflow.com/questions/46013287/c-sharp-global-keyboard-hook-that-opens-a-form-from-a-console-application
    
    Mainly used while playing PUBG, program listens to F5-F8 Key presses
    
    F5 - Increment Win counter
    F6 - Increment Kill counter
    F7 - Increment KO counter
    F8 - Increment Loss counter (Most used)

---
+++ Test2/Test2/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Test2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 42);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(478, 396);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(335, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Hook";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(416, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Unhook";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Hook Status:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "OFF";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Test2/Test2/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Test2
+{
+    public partial class Form1 : Form
+    {
+        const string stats_file = @"C:\Users\ecalf\Desktop\PUBG_Stats\pubg_stats.txt";
+
+        LowLevelKeyboardHook kbh;
+
+        int t_wins = 0;
+        int t_kills = 0;
+        int t_kos = 0;
+        int t_loss = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Initiate Keyboard Hook
+            kbh = new LowLevelKeyboardHook();
+            kbh.OnKeyPressed += Kbh_OnKeyPressed;
+            kbh.OnKeyUnpressed += Kbh_OnKeyUnpressed;
+
+            // Load data
+            string[] lines = System.IO.File.ReadAllLines(stats_file);
+
+            // Output current stats to textbox
+            RichTextNewLine(lines[0]);
+            RichTextNewLine(lines[1]);
+            RichTextNewLine(lines[2]);
+            RichTextNewLine(lines[3]);
+            RichTextNewLine(lines[4]);
+
+            // Update internal ints with values from file
+            t_wins = Int32.Parse(lines[1].Split()[2]);
+            t_kills = Int32.Parse(lines[2].Split()[2]);
+            t_kos = Int32.Parse(lines[3].Split()[2]);
+            t_loss = Int32.Parse(lines[4].Split()[2]);
+        }
+
+        // Do something on keypress
+        private void Kbh_OnKeyPressed(object sender, Keys e)
+        {
+            if (e == Keys.F5)
+            {
+                RichTextNewLine("+Win");
+                t_wins++;
+            }
+            else if (e == Keys.F6)
+            {
+                RichTextNewLine("+Kill");
+                t_kills++;
+            }
+            else if (e == Keys.F7)
+            {
+                RichTextNewLine("+KO");
+                t_kos++;
+            }
+            else if (e == Keys.F8)
+            {
+                RichTextNewLine("+Loss");
+                t_loss++;
+            }
+
+            WriteStats();
+        }
+
+        // Write stats to file
+        private void WriteStats()
+        {
+            string[] to_write = {
+                "Today: " + DateTime.Now.ToString("M/d/yyyy"),
+                "Total Wins: " + t_wins,
+                "Total Kills: " + t_kills,
+                "Total KO's: " + t_kos,
+                "Total Losses: " + t_loss
+                };
+
+            System.IO.File.WriteAllLines(stats_file, to_write);
+        }
+
+        // AppendText to TextBox + Newline
+        void RichTextNewLine(string s)
+        {
+            richTextBox1.AppendText(s + '\n');
+        }
+
+        // Do nothing when key unpressed
+        private void Kbh_OnKeyUnpressed(object sender, Keys e)
+        {
+            // Do nothing
+        }
+
+        // Hook button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            kbh.HookKeyboard();
+            label2.Text = "ON";
+            RichTextNewLine("Hooked");
+        }
+
+        // Unhook button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            kbh.UnHookKeyboard();
+            label2.Text = "OFF";
+            RichTextNewLine("Unhooked");
+        }
+    }
+
+    public class LowLevelKeyboardHook
+    {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_SYSKEYDOWN = 0x0104;
+        private const int WM_KEYUP = 0x101;
+        private const int WM_SYSKEYUP = 0x105;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        public delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        public event EventHandler<Keys> OnKeyPressed;
+        public event EventHandler<Keys> OnKeyUnpressed;
+
+        private LowLevelKeyboardProc _proc;
+        private IntPtr _hookID = IntPtr.Zero;
+
+        public LowLevelKeyboardHook()
+        {
+            _proc = HookCallback;
+        }
+
+        public void HookKeyboard()
+        {
+            _hookID = SetHook(_proc);
+        }
+
+        public void UnHookKeyboard()
+        {
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN || wParam == (IntPtr)WM_SYSKEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+
+                OnKeyPressed.Invoke(this, ((Keys)vkCode));
+            }
+            else if (nCode >= 0 && wParam == (IntPtr)WM_KEYUP || wParam == (IntPtr)WM_SYSKEYUP)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+
+                OnKeyUnpressed.Invoke(this, ((Keys)vkCode));
+            }
+
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+    }
+}
